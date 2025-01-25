c67ddfb87f74b5a28cec5d1f0b01f2ced7a9f65e
Adam Ornacki <147385571+RustyRac@users.noreply.github.com>
2025-01-25 20:30:25 +0100

    Add files via upload

---
+++ BST Tree/BST Tree/Program.cs
@@ -0,0 +1,208 @@
+﻿using System;
+
+class Node
+{
+    public int Value;
+    public Node Left;
+    public Node Right;
+
+    public Node(int value)
+    {
+        Value = value;
+        Left = Right = null;
+    }
+}
+
+class BST
+{
+    public Node Root;
+
+    public BST()
+    {
+        Root = null;
+    }
+
+   // Dodawanie
+    public void Insert(int value)
+    {
+        Root = Inserter(Root, value);
+    }
+
+
+    private Node Inserter(Node root, int value)
+    {
+      
+        if (root == null)
+        {
+            root = new Node(value);
+            return root;
+        }
+
+      
+        if (value < root.Value)
+        {
+            root.Left = Inserter(root.Left, value);
+        }
+        else if (value > root.Value)
+        {
+            root.Right = Inserter(root.Right, value);
+        }
+
+      
+        return root;
+    }
+
+    // In-Order
+    public void InOrder()
+    {
+        InOrderator(Root);
+    }
+
+    private void InOrderator(Node root)
+    {
+        if (root != null)
+        {
+            InOrderator(root.Left);
+            Console.Write(root.Value + " ");
+            InOrderator(root.Right);
+        }
+    }
+
+    //Pre-Order
+    public void PreOrder()
+    {
+        PreOrderator(Root);
+    }
+
+    private void PreOrderator(Node root)
+    {
+        if (root != null)
+        {
+            Console.Write(root.Value + " ");
+            PreOrderator(root.Left);
+            PreOrderator(root.Right);
+        }
+    }
+
+    // Post-Order
+    public void PostOrder()
+    {
+        PosrOrderator(Root);
+    }
+
+    private void PosrOrderator(Node root)
+    {
+        if (root != null)
+        {
+            PosrOrderator(root.Left);
+            PosrOrderator(root.Right);
+            Console.Write(root.Value + " ");
+        }
+    }
+
+    //Znajdywanie
+    public bool Search(int value)
+    {
+        return Finder(Root, value);
+    }
+
+    private bool Finder(Node root, int value)
+    {
+        if (root == null)
+            return false;
+
+        if (root.Value == value)
+            return true;
+
+        if (value < root.Value)
+            return Finder(root.Left, value);
+
+        return Finder(root.Right, value);
+    }
+
+    // Usuwanie
+    public void Delete(int value)
+    {
+        Root = Deletator(Root, value);
+    }
+
+    private Node Deletator(Node root, int value)
+    {
+        
+        if (root == null)
+            return root;
+
+       
+        if (value < root.Value)
+            root.Left = Deletator(root.Left, value);
+        else if (value > root.Value)
+            root.Right = Deletator(root.Right, value);
+        else
+        {
+            
+            if (root.Left == null)
+                return root.Right;
+            else if (root.Right == null)
+                return root.Left;
+
+            
+            root.Value = MinValue(root.Right);
+
+            
+            root.Right = Deletator(root.Right, root.Value);
+        }
+
+        return root;
+    }
+
+    //Znajdywanie najmniejszego
+    private int MinValue(Node root)
+    {
+        int minValue = root.Value;
+        while (root.Left != null)
+        {
+            minValue = root.Left.Value;
+            root = root.Left;
+        }
+        return minValue;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        BST tree = new BST();
+
+        
+        tree.Insert(8);
+        tree.Insert(7);
+        tree.Insert(12);
+        tree.Insert(13);
+        tree.Insert(1);
+        tree.Insert(9);
+        tree.Insert(12);
+
+        Console.WriteLine("In-order:");
+        tree.InOrder();
+
+        Console.WriteLine("\nPre-order:");
+        tree.PreOrder();
+
+        Console.WriteLine("\nPost-order:");
+        tree.PostOrder();
+
+        Console.WriteLine("\nSzukanie 13");
+        Console.WriteLine(tree.Search(13));
+
+        Console.WriteLine("\nUsuwanie 7");
+        tree.Delete(7);
+        tree.InOrder();
+
+        Console.WriteLine("\nUsuwanie 1");
+        tree.Delete(1);
+        tree.InOrder();
+
+      
+    }
+}
---
+++ Dijkstra/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TreeView;
+
+namespace Dijkstra
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            int V = 8;
+            Graph graph = new Graph(V);
+
+            graph.AddEdge(0, 1, 5);
+            graph.AddEdge(0, 7, 8);
+            graph.AddEdge(1, 2, 4);
+            graph.AddEdge(1, 7, 2);
+            graph.AddEdge(2, 3, 7);
+            graph.AddEdge(2, 5, 5);
+            graph.AddEdge(3, 4, 9);
+            graph.AddEdge(3, 5, 14);
+            graph.AddEdge(4, 5, 10);
+            graph.AddEdge(5, 6, 2);
+            graph.AddEdge(6, 7, 6);           
+
+
+            graph.Dijkstra(0);
+        }
+    }
+}
