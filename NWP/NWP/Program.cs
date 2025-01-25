c1727d7cd0f9bb8986053b7b4f52a482c4b22e2c
Adam Ornacki <147385571+RustyRac@users.noreply.github.com>
2025-01-25 20:31:43 +0100

    Add files via upload

---
+++ NWP/NWP/Program.cs
@@ -0,0 +1,84 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    public static void MatrixPrinter(int[,] array)
+    {
+        int rows = array.GetLength(0);  
+        int cols = array.GetLength(1);  
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write(array[i, j] + " ");  
+            }
+            Console.WriteLine();
+        }
+    }
+    public static string NWP(string str1, string str2)
+    {
+        int len1 = str1.Length;
+        int len2 = str2.Length;
+
+        
+        int[,] matrix = new int[len1 + 1, len2 + 1];
+
+       
+        for (int i = 1; i <= len1; i++)
+        {
+            for (int j = 1; j <= len2; j++)
+            {
+                if (str1[i - 1] == str2[j - 1]) 
+                {
+                    matrix[i, j] = matrix[i - 1, j - 1] + 1;
+                }
+                else  
+                {
+                    matrix[i, j] = Math.Max(matrix[i - 1, j], matrix[i, j - 1]);
+                }
+            }
+        }
+
+        MatrixPrinter(matrix);
+
+        int lcsLength = matrix[len1, len2];
+        char[] lcs = new char[lcsLength];
+        int x = len1, y = len2;
+        while (x > 0 && y > 0)
+        {
+            if (str1[x - 1] == str2[y - 1])
+            {
+                lcs[lcsLength - 1] = str1[x - 1];
+                x--;
+                y--;
+                lcsLength--;
+            }
+            else if (matrix[x - 1, y] >= matrix[x, y - 1])
+            {
+                x--;
+            }
+            else
+            {
+                y--;
+            }
+        }
+
+        return new string(lcs);
+    }
+
+    static void Main()
+    {
+
+        string str1 = "ABAB";
+        Console.WriteLine(str1);
+
+        string str2 = "ABGBAGB";
+        Console.WriteLine(str2);
+
+
+
+        string nwp = NWP(str1, str2);
+        Console.WriteLine("NWP:  " + nwp);
+    }
+}
---
+++ Przechodzenie przez graf/Przechodzenie przez graf/Program.cs
@@ -0,0 +1,96 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+class Graph
+{
+    private Dictionary<int, List<int>> adjacencyList;
+
+    public Graph()
+    {
+        adjacencyList = new Dictionary<int, List<int>>();
+    }
+
+    // Dodanie krawędzi do grafu
+    public void AddEdge(int u, int v)
+    {
+        if (!adjacencyList.ContainsKey(u))
+        {
+            adjacencyList[u] = new List<int>();
+        }
+        adjacencyList[u].Add(v);
+    }
+
+    // Przechodzenie grafu w głąb (DFS)
+    public void DFS(int start)
+    {
+        bool[] visited = new bool[adjacencyList.Count + 1];
+        DFSUtil(start, visited);
+    }
+
+    // Pomocnicza metoda DFS (rekursywna)
+    private void DFSUtil(int node, bool[] visited)
+    {
+        // Odwiedź węzeł
+        Console.WriteLine(node);
+        visited[node] = true;
+
+        // Rekurencyjne przechodzenie po sąsiadach
+        foreach (var neighbor in adjacencyList[node])
+        {
+            if (!visited[neighbor])
+            {
+                DFSUtil(neighbor, visited);
+            }
+        }
+    }
+
+    // Przechodzenie grafu w szerz (BFS) bez użycia kolejki
+    public void BFS(int start)
+    {
+        bool[] visited = new bool[adjacencyList.Count + 1];
+        List<int> list = new List<int>();
+
+        // Rozpocznij od węzła początkowego
+        visited[start] = true;
+        list.Add(start);
+
+        int index = 0;
+        while (index < list.Count)
+        {
+            // Usuwamy pierwszy element z listy (symulacja działania kolejki)
+            int node = list[index];
+            index++;
+            Console.WriteLine(node);
+
+            // Przejdź po sąsiadach węzła
+            foreach (var neighbor in adjacencyList[node])
+            {
+                if (!visited[neighbor])
+                {
+                    visited[neighbor] = true;
+                    list.Add(neighbor);
+                }
+            }
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Graph graph = new Graph();
+        graph.AddEdge(1, 2);
+        graph.AddEdge(1, 3);
+        graph.AddEdge(2, 4);
+        graph.AddEdge(2, 5);
+        graph.AddEdge(3, 6);
+
+        Console.WriteLine("Przechodzenie grafu w głąb (DFS):");
+        graph.DFS(1);
+
+        Console.WriteLine("\nPrzechodzenie grafu w szerz (BFS):");
+        graph.BFS(1);
+    }
+}
